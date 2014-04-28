107f7a870c9478989a2d9c7e12dc039022d2859f
Diego <songecko@gmail.com>
2014-04-28 18:39:36 -0300

    some changes

--- src/Warlikes/Resources/views/Main/index.php
+++ src/Warlikes/Resources/views/Main/index.php
@@ -1,7 +1,8 @@
 <div id="home">
+	<a href="<?php echo $loginUrl ?>" target="_top"><span>Ingresar</span></a>
+	<a href="<?php echo $loginUrl ?>" target="_top" class="calltoaction">Participa</a>
 	<div class="text">
 		<p>La ropa de entrecasa es la mejor que tenemos. Exacto, es la mejor que tenemos, pero para estar en casa. No en Facebook. Solo en casa nos animamos a poner unas pantuflas de oso, una remera extremadamente apretada, o una bata rosa. Esa ropa no se comparte, no se muestra publicamente. A lo sumo la ve algún familiar y algún que otro de esos vecinos que les gusta espiar por la ventana.</p>
 		<p>Por eso easy hoy premia a los que comparten la ropa de entrecasa. Pero en este caso, no a los que tienen más likes, sino a los que tienen MENOS likes.</p>
 	</div>
-	<a href="<?php echo $loginUrl ?>" target="_top"><span>Ingresar</span></a>
 </div>
